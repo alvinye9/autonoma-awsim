4c2866d630206502281b317848efa3619c42db2b
Alvin Ye <alvinye9@gmail.com>
2024-06-23 01:06:18 -0400

    cleaned up kentucky spdwy implementation

--- Scripts/Framework/SpawnManager.cs
+++ Scripts/Framework/SpawnManager.cs
@@ -485,65 +485,14 @@ public class SpawnManager : MonoBehaviour
             GameObject track = myLoadedAssetBundle.LoadAsset<GameObject>(trackName); //trackName is read from tracklist
             Debug.Log("Enable Novatel Top: " + GameManager.Instance.Settings.mySensorSet.EnableTop);
 
-            
             if(trackName.Equals("LVMS.prefab"))
             {
                 if(GameManager.Instance.Settings.myTrackParams.TrackName == "Kentucky Speedway")
                 {
                     Debug.Log("INSTANTIATING KENTUCKY SPEEDWAY TRACK");
-//CCW is negatibe
-//CW is positive
-                    // Vector3 position = new Vector3(458f, -2.1f, -357f);  
-                    // Quaternion rotation = Quaternion.Euler(0f, 127f, 0f);  
-                    // Vector3 scale = new Vector3(1.0f, 1.15f, 1.0f); //scale too big
-
-                    // Vector3 position = new Vector3(435f, -2.0f, -340f);   //too low, need increase rotation angle
-                    // Quaternion rotation = Quaternion.Euler(0f, 127f, 0f);  
-                    // Vector3 scale = new Vector3(0.95f, 1.0f, 0.95f);
-
-                    // Vector3 position = new Vector3(429f, -2.1f, -346f);  
-                    // Quaternion rotation = Quaternion.Euler(0f, 128f, 0f);  
-                    // Vector3 scale = new Vector3(0.95f, 1.15f, 0.95f); 
-
-                    // Vector3 position = new Vector3(440f, -2.0f, -355f);  
-                    // Quaternion rotation = Quaternion.Euler(0f, 128f, 0f);  //need to rotate more ccw (127.5?)
-                    // Vector3 scale = new Vector3(0.97f, 1.0f, 0.97f);
-
-                    // Vector3 position = new Vector3(443f, -2.0f, -350f);  
-                    // Quaternion rotation = Quaternion.Euler(0f, 127.5f, 0f);  //seems like 127 may be better
-                    // Vector3 scale = new Vector3(0.97f, 1.0f, 0.97f);
-
-                    // Vector3 position = new Vector3(441f, -2.0f, -343f);   //too low, need increase rotation angle (127.5??)
-                    // Quaternion rotation = Quaternion.Euler(0f, 127f, 0f);  
-                    // Vector3 scale = new Vector3(0.96f, 1.0f, 0.96f); //size too big? 
-
-                    // Vector3 position = new Vector3(433f, -2.0f, -343f);   
-                    // Quaternion rotation = Quaternion.Euler(0f, 127.5f, 0f);  //more CCW (127?)
-                    // Vector3 scale = new Vector3(0.95f, 1.0f, 0.95f); //size is right
-
-                    // Vector3 position = new Vector3(441f, -2.0f, -339f);   
-                    // Quaternion rotation = Quaternion.Euler(0f, 126.5f, 0f);  
-                    // Vector3 scale = new Vector3(0.96f, 1.0f, 0.96f); //size too big?
-
-                    // Vector3 position = new Vector3(439f, -2.0f, -346f);   
-                    // Quaternion rotation = Quaternion.Euler(0f, 127.5f, 0f); 
-                    // Vector3 scale = new Vector3(0.96f, 1.0f, 0.96f); 
-
-                    // Vector3 position = new Vector3(435f, -2.0f, -350f);  
-                    // Quaternion rotation = Quaternion.Euler(0f, 128f, 0f);  
-                    // Vector3 scale = new Vector3(0.96f, 1.0f, 0.96f);
-
-                    // Vector3 position = new Vector3(430f, -2.0f, -358f);  
-                    // Quaternion rotation = Quaternion.Euler(0f, 129f, 0f);  
-                    // Vector3 scale = new Vector3(0.96f, 1.0f, 0.96f);
-
-
-                    // Vector3 position = new Vector3(432f, -2.3f, -343f); //works
-                    // Vector3 scale = new Vector3(0.97f, 1.0f, 0.95f); 
 
                     Vector3 position = new Vector3(440f, -1.9f, -342f);
                     Vector3 scale = new Vector3(0.97f, 1.08f, 0.96f); 
-
                     Quaternion rotation = Quaternion.Euler(0f, 129f, 0f); 
                     GameObject instantiatedTrack = Instantiate(kentuckyPrefab, position, rotation); //for new prefabs
                     instantiatedTrack.transform.localScale = scale;
@@ -558,7 +507,6 @@ public class SpawnManager : MonoBehaviour
                     GameObject instantiatedTrack = Instantiate(track, position, rotation);
                     instantiatedTrack.transform.localScale = scale;
                 }
-
             }
 
             else
